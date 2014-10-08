5169f2db7e05ea7b406ac5b90afb4e4dc10610cf
Bryson Tyrrell <brysontyrrell@users.noreply.github.com>
2014-10-08 14:35:47 -0500

    Update managementaccount.sh
    modified to use Mac's UUID for API calls, returning the General subset and simplified reading out the XML attribute

--- managementaccount.sh
+++ managementaccount.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
-# managementaccount.sh
 #
 # queries API to find the management account for a computer, meant to assist with FileVault 2 preparations by displaying the account in an inventory search and to be used for scoping policies
-#
 # by @emilyooo, 2014-10-08
+#
+# modified to use Mac's UUID for API calls, returning the General subset and simplified reading out the XML attribute
 
+uuid=$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/Hardware UUID/ {print $3}')
 
+value=$(/usr/bin/curl -s -u "$username":"$password" https://jss.address.com/JSSResource/computers/udid/$uuid/subset/General | /usr/bin/xpath "//remote_management/management_username/text()" 2> /dev/null)
 
-value=$(curl -s -u user:password https://companyjss:8443/JSSResource/computers/macaddress/$(networksetup -getmacaddress en0 | awk '{print $3}' | sed 's/:/./g') | xpath "//computer/general/remote_management/management_username[1]" | sed -e 's/<management_username>//;s/<\/management_username>//')
 echo "<result>$value</result>"
